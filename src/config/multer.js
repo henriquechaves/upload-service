4cb6ae694fc97738d5cc977c86b0c6d89a0f5f8e
henriquechaves <henrique.chaves@queimadiaria.com.br>
2019-01-29 11:35:26 -0200

    Ajustes configuracionais

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,14 +4,24 @@ const crypto = require('crypto');
 
 
 module.exports = {
-    dest: path.resolve(__dirname, '..','..', 'temp', 'uploads'),
-    storage: multer.diskStorage({
+  dest: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
+  storage: multer.diskStorage({}),
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
+    fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+          'image/jpeg',
+          'image/pjpeg',
+          'image/png',
+        'image/gif'
+        ];
 
-    }),
-    limits: {
-        fileSize: 2 * 1024 * 1024
-    },
-    fileFilter: () => {
-        
-    },
+        if(allowedMimes.includes(file.mimetype)) {
+            cb(null, true)
+        } else {
+            cb( new error('Invalid file type'))
+        }
+
+    }
 };
